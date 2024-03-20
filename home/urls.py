7ad190c0a617ba1c8c729c2908163f349c1443f7
agiac16 <agiac16@pm.me>
2024-03-20 16:10:53 -0400

    homepage almost done, herbert login signup

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("", views.homepage, name='homepage'),
+    path("signup/", views.signup, name='signup'),
+    path('login/', views.login, name='login')
 ]
+
